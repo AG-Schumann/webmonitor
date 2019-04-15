542c6eb90dd327ccea69520bf9898e413213e7eb
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2019-04-15 14:57:12 +0200

    Deployed!

--- webmonitor/wsgi.py
+++ webmonitor/wsgi.py
@@ -15,6 +15,11 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append('/software/webmonitor')
 sys.path.append('/software/anaconda3/envs/server/lib/python3.7/site-packages')
 
+python_home = '/software/anaconda3/envs/server'
+activate_this = python_home + '/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webmonitor.settings')
 
 application = get_wsgi_application()
